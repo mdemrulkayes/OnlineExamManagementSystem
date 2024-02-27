ff1727009bb7f6ec83175660c00bba22090087c7
Emrul Kayes <emrul@blockbonds.io>
2024-02-27 12:14:19 +0600

    add new API project and use SQL server instead of Postgre

--- src/Modules/Identity/Modules.Identity/ServiceCollectionExtensions.cs
+++ src/Modules/Identity/Modules.Identity/ServiceCollectionExtensions.cs
@@ -1,14 +1,23 @@
 ﻿using FluentValidation;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Modules.Identity.Persistence;
 using System.Reflection;
+using Modules.Identity.Entities;
 
 namespace Modules.Identity;
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection RegisterFluentValidation(this IServiceCollection services)
+    public static IServiceCollection RegisterIdentityModule(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .RegisterFluentValidation()
+            .RegisterIdentityDatabase(configuration);
+        return services;
+    }
+    private static IServiceCollection RegisterFluentValidation(this IServiceCollection services)
     {
         ValidatorOptions.Global.DefaultClassLevelCascadeMode = CascadeMode.Continue;
         ValidatorOptions.Global.DefaultRuleLevelCascadeMode = CascadeMode.Stop;
@@ -16,16 +25,39 @@ public static class ServiceCollectionExtensions
 
         return services;
     }
-    public static IServiceCollection RegisterIdentityDatabase(this IServiceCollection services, IConfiguration configuration)
+    private static IServiceCollection RegisterIdentityDatabase(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<IdentityModuleDbContext>(opt =>
         {
-            opt.UseNpgsql(configuration.GetConnectionString("IdentityModuleDbContext"));
+            opt.UseSqlServer(configuration.GetConnectionString("IdentityModuleDbContext"));
         });
 
-       MigrateDatabase(services);
+        RegisterIdentity(services);
+
+        MigrateDatabase(services);
 
         return services;
+    }
+
+    private static void RegisterIdentity(IServiceCollection services)
+    {
+        services.Configure<IdentityOptions>(options =>
+        {
+            options.Password.RequireDigit = true;
+            options.Password.RequireLowercase = true;
+            options.Password.RequireNonAlphanumeric = true;
+            options.Password.RequireUppercase = true;
+            options.Password.RequiredLength = 6;
+            options.Password.RequiredUniqueChars = 1;
+            options.User.RequireUniqueEmail = false;
+            options.SignIn.RequireConfirmedEmail = false;
+            options.SignIn.RequireConfirmedAccount = false;
+            options.SignIn.RequireConfirmedPhoneNumber = false;
+        });
+
+        services.AddIdentityCore<ApplicationUser>()
+            .AddEntityFrameworkStores<IdentityModuleDbContext>()
+            .AddDefaultTokenProviders();
     }
 
     private static void MigrateDatabase(IServiceCollection services)
