b21127c4f8a95466a13bfe296b00be0eb338bba3
Emrul Kayes <emrul.kayes2103@gmail.com>
2024-11-03 22:44:33 +0600

    remove unnecessary import

--- src/API/Quizzer.Api/Program.cs
+++ src/API/Quizzer.Api/Program.cs
@@ -1,6 +1,4 @@
 using System.Reflection;
-using Elastic.Ingest.Elasticsearch;
-using Elastic.Ingest.Elasticsearch.DataStreams;
 using Elastic.Serilog.Sinks;
 using Modules.Identity;
 using Modules.Quiz.Endpoints;
